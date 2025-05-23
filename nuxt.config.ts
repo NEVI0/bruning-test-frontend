dd9040c18793513beee2b017f6cd7441a0f5d3d6
NEVI0 <neviocostamagagnin@gmail.com>
2025-05-22 21:21:23 -0300

    feat: adding project configurations

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  srcDir: 'src',
+  css: ['~/assets/css/main.css'],
+
+  app: {
+    head: {
+      title: 'Bruning Sistemas',
+      htmlAttrs: {
+        lang: 'pt-BR',
+      },
+      charset: 'utf-16',
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+    },
+  },
+
+  devtools: {
+    enabled: true,
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  modules: ['@nuxt/icon'],
+});
