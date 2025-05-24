39bc76a051d366728cdacaaf9ad943c8f804e8cb
NEVI0 <neviocostamagagnin@gmail.com>
2025-05-24 15:56:54 -0300

    feat: adding employee implementation correctly

--- infra/repositories/ApiEmployeeRepository.ts
+++ infra/repositories/ApiEmployeeRepository.ts
@@ -1,3 +1,4 @@
+import type { EmployeeAbstract } from '~~/domain/entities';
 import type { HttpProviderAbstract } from '~~/domain/providers';
 import type { EmployeeRepository } from '~~/domain/repositories';
 
@@ -5,18 +6,51 @@ export default class ApiEmployeeRepository implements EmployeeRepository {
   constructor(private readonly httpProvider: HttpProviderAbstract) {}
 
   public findAll: EmployeeRepository['findAll'] = async () => {
-    return Promise.resolve(null as any);
+    const response = await this.httpProvider.get<EmployeeAbstract[]>(
+      '/employees'
+    );
+
+    return response;
   };
 
-  public create: EmployeeRepository['create'] = async () => {
-    return Promise.resolve(null as any);
+  public create: EmployeeRepository['create'] = async (employee) => {
+    const payload = {
+      id: employee.id,
+      name: employee.name,
+      nickname: employee.nickname,
+      parents: employee.parents,
+      document: employee.document,
+      birthdate: employee.birthdate,
+      jobDate: employee.jobDate,
+    };
+
+    const response = await this.httpProvider.post<EmployeeAbstract>(
+      '/employees',
+      payload
+    );
+
+    return response;
   };
 
-  public update: EmployeeRepository['update'] = async () => {
-    return Promise.resolve(null as any);
+  public update: EmployeeRepository['update'] = async (employee) => {
+    const payload = {
+      name: employee.name,
+      nickname: employee.nickname,
+      parents: employee.parents,
+      document: employee.document,
+      birthdate: employee.birthdate,
+      jobDate: employee.jobDate,
+    };
+
+    const response = await this.httpProvider.put<EmployeeAbstract>(
+      `/employees/${employee.id}`,
+      payload
+    );
+
+    return response;
   };
 
-  public deleteById: EmployeeRepository['deleteById'] = async () => {
-    return Promise.resolve(null as any);
+  public deleteById: EmployeeRepository['deleteById'] = async (id) => {
+    await this.httpProvider.delete(`/employees/${id}`);
   };
 }
