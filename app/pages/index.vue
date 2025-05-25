ff3956f7656f2583dd364bbe0e0797dcb118dc7b
NEVI0 <neviocostamagagnin@gmail.com>
2025-05-25 09:53:51 -0300

    feat: making the home page functional

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,28 +1,34 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
+
+import type { EmployeeAbstract } from '~~/domain/entities';
+import {
+  makeFindAllEmployeesUseCase,
+  makeDeleteEmployeeByIdUseCase,
+} from '~~/factories/useCases';
 
 const router = useRouter();
 
-const employees = ref([
-  { id: 1, name: 'John Doe', birthday: '10/01/1990' },
-  { id: 2, name: 'Jane Doe', birthday: '10/01/1990' },
-  { id: 3, name: 'John Doe', birthday: '10/01/1990' },
-  { id: 4, name: 'Jane Doe', birthday: '10/01/1990' },
-]);
+const isLoading = ref(true);
+const hasError = ref(false);
+const employees = ref<EmployeeAbstract[]>([]);
 
-const sampleData = ref({
-  code: '123',
-  name: 'John Doe',
-  nickname: 'Johnny',
-  father: 'James Doe',
-  mother: 'Jane Doe',
-  document: '123.456.789-00',
-  birthday: '01/01/1990',
-  job: 'Developer',
-});
+const handleFetchEmployees = async () => {
+  try {
+    isLoading.value = true;
+    hasError.value = false;
 
-const goToForm = (employee: (typeof employees.value)[0]) => {
+    const employees = await makeFindAllEmployeesUseCase().execute();
+    this.employees = employees;
+  } catch (error) {
+    hasError.value = true;
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+const handleUpdateEmployee = (employee: (typeof employees.value)[0]) => {
   router.push({
     path: '/form',
     query: {
@@ -35,40 +41,76 @@ const goToForm = (employee: (typeof employees.value)[0]) => {
   });
 };
 
-const removeEmployee = (id: number) => {
-  employees.value = employees.value.filter((employee) => employee.id !== id);
-};
-
-const addEmployee = (employee: (typeof employees.value)[0]) => {
-  employees.value.push(employee);
-};
-
-const updateEmployee = (
-  id: number,
-  updatedData: Partial<(typeof employees.value)[0]>
-) => {
-  const index = employees.value.findIndex((emp) => emp.id === id);
-  if (index !== -1) {
-    employees.value[index] = { ...employees.value[index], ...updatedData };
+const handleRemoveEmployee = async (id: string) => {
+  try {
+    await makeDeleteEmployeeByIdUseCase().execute({ id });
+    handleFetchEmployees();
+  } catch (error) {
+    console.error('Error deleting employee:', error);
   }
 };
+
+onMounted(() => {
+  handleFetchEmployees();
+});
 </script>
 
 <template>
-  <section class="flex flex-col gap-2">
-    <h2 class="text-2xl font-bold">Listagem de colaboradores</h2>
-    <h3 class="text-gray-500 text-xl font-bold">
-      Total de colaboradores: {{ employees.length }}
-    </h3>
+  <section
+    v-if="isLoading"
+    class="flex flex-col items-center justify-center flex-1 gap-8"
+  >
+    <Lottie name="searching" width="300px" height="150px" loop="true" />
+
+    <div
+      class="flex flex-col items-center justify-center gap-2 w-full md:w-1/2"
+    >
+      <h1 class="text-center text-2xl font-bold">Carregando...</h1>
+
+      <p class="text-center text-gray-600">
+        Buscando colaboradores cadastrados, aguarde alguns instantes...
+      </p>
+    </div>
   </section>
 
-  <section class="flex flex-col gap-4">
-    <Employee
-      v-for="employee in employees"
-      :key="employee.id"
-      :employee="employee"
-      @edit="goToForm"
-      @remove="removeEmployee"
-    />
+  <section
+    v-else-if="hasError || !employees.length"
+    class="flex flex-col items-center justify-center flex-1 gap-8"
+  >
+    <Lottie name="search" width="200px" height="200px" loop="false" />
+
+    <div
+      class="flex flex-col items-center justify-center gap-2 w-full md:w-1/2"
+    >
+      <h1 class="text-center text-2xl font-bold">Ops... nada encontrado!</h1>
+
+      <p class="text-center text-gray-600">
+        Não foi possivel encontrar os colaboradores cadastrados. Clique no botão
+        abaixo para tentar novamente.
+      </p>
+    </div>
+
+    <div class="flex items-center justify-center gap-4">
+      <Button @click="handleFetchEmployees">Tentar novamente</Button>
+    </div>
   </section>
+
+  <template v-else>
+    <section class="flex flex-col gap-2">
+      <h2 class="text-2xl font-bold">Listagem de colaboradores</h2>
+      <h3 class="text-gray-500 text-xl font-bold">
+        Total de colaboradores: {{ employees.length }}
+      </h3>
+    </section>
+
+    <section class="flex flex-col gap-4">
+      <Employee
+        v-for="employee in employees"
+        :key="employee.id"
+        :employee="employee"
+        @edit="handleUpdateEmployee"
+        @remove="handleRemoveEmployee"
+      />
+    </section>
+  </template>
 </template>
